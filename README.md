73129aa17e60564d0e579857921b9aacc7c0fe92
The Meta <alexmeta517@gmail.com>
2023-02-22 14:34:10 +0300

    Initial Commit

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 ## Leason One - Binary Search, Linked List And Complexity
 
+___
 
+- Understand the question asked by the interviewer
+- 
--- binary_search.py
+++ binary_search.py
@@ -23,124 +23,110 @@ def locate_card(cards, query):
             #We return -1
             return -1  
 
-    #lets define test cases
-
-    #Letscreate a big test list
-    tests = []
-
-    #Respresent the test cases as dictionaries
-    test = {
-        'input': {
-            'cards':[13,11,10,7,4,3,1,0],
-            'query': 7
-        },
-        'output': 3
-    }
-
-    #Append the tests
-    tests.append(test)
-
-    #Query occurs in the middle
-    test1 = {
-        'input': {
-            'cards':[13,11,10,7,4,3,1,0],
-            'query': 7
-        },
-        'output': 3
-    }
-
-    #Append the tests
-    tests.append(test1)
-
-    #Query is first element
-    test2 = {
-        'input': {
-            'cards':[4,2,1,-1],
-            'query': 4
-        },
-        'output': 0
-    }
-
-    #Append the tests
-    tests.append(test2)
-
-    #Query is the last element
-    test3 = {
-        'input': {
-            'cards':[3,-1,-9,-127],
-            'query': -127
-        },
-        'output': 0
-    }
-
-    #Append the tests
-    tests.append(test3)
-
-    #Query contains one element
-    test4 = {
-        'input': {
-            'cards':[6],
-            'query': 6
-        },
-        'output': 0
-    }
-
-    #Append the tests
-    tests.append(test4)
-
-    #Card doesnt contain the query
-    #Assume it will return -1
-
-    test5 = {
-        'input': {
-            'cards':[9,7,5,2,-9],
-            'query': 4
-        },
-        'output': -1
-    }
-
-    
-    #Append the tests
-    tests.append(test5)
-
-    #The cards is empty
-    test6 = {
-        'input': {
-            'cards':[],
-            'query': 7
-        },
-        'output': -1
-    }
-
-    
-    #Append the tests
-    tests.append(test6)
-
-    #The cards have repeating numbers
-    test7 = {
-        'input': {
-            'cards':[8,8,6,6,6,6,6,3,2,2,2,0,0,0],
-            'query': 3
-        },
-        'output': 7
-    }
 
 
-    #Append the tests
-    tests.append(test7)
+#lets define test cases
+#Lets create a big test list
+tests = []
 
-    #When query occurs many times
-    test8 = {
-        'input': {
-            'cards':[8,8,6,6,6,6,6,3,2,2,2,0,0,0],
-            'query': 6
-        },
-        'output': 2
-    }
+#Respresent the test cases as dictionaries
+test = {
+    'input': {
+        'cards':[13,11,10,7,4,3,1,0],
+        'query': 7
+    },
+    'output': 3
+}
 
+#Append the tests
+tests.append(test)
 
-    #Append the tests
-    tests.append(test8)
+#Query occurs in the middle
+#Append the tests
+tests.append({
+    'input': {
+        'cards':[13,11,10,7,4,3,1,0],
+        'query': 7
+    },
+    'output': 3
+}
+)
 
-    #Look at the test cases
-    print(tests)
+#Query is first element
+#Append the tests
+tests.append({
+    'input': {
+        'cards':[4,2,1,-1],
+        'query': 4
+    },
+    'output': 0
+})
+
+#Query is the last element
+#Append the tests
+tests.append({
+    'input': {
+        'cards':[3,-1,-9,-127],
+        'query': -127
+    },
+    'output': 0
+})
+
+#Query contains one element
+#Append the tests
+tests.append({
+    'input': {
+        'cards':[6],
+        'query': 6
+    },
+    'output': 0
+})
+
+#Card doesnt contain the query
+#Assume it will return -1
+#Append the tests
+tests.append({
+    'input': {
+        'cards':[9,7,5,2,-9],
+        'query': 4
+    },
+    'output': -1
+})
+
+#The cards is empty
+#Append the tests
+tests.append({
+    'input': {
+        'cards':[],
+        'query': 7
+    },
+    'output': -1
+}
+)
+
+#The cards have repeating numbers
+#Append the tests
+tests.append({
+    'input': {
+        'cards':[8,8,6,6,6,6,6,3,2,2,2,0,0,0],
+        'query': 3
+    },
+    'output': 7
+})
+
+#When query occurs many times
+#Append the tests
+tests.append({
+    'input': {
+        'cards':[8,8,6,6,6,6,6,3,2,2,2,0,0,0],
+        'query': 6
+    },
+    'output': 2
+})
+
+#Lets test using the first test case
+result = locate_card(test['input']['cards'], test['input']['query'])
+
+print(result == test['output'])
+
+#Now we test all our test cases
