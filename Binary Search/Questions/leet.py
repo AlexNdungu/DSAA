0ed1b727200c507d80c2f9124552a03c0d17ade5
The Meta <alexmeta517@gmail.com>
2023-02-22 19:14:11 +0300

    Test Cases Defined

--- Binary Search/Questions/leet.py
+++ Binary Search/Questions/leet.py
@@ -1,4 +1,4 @@
-#LeetCode Question
+#LeetCode Question1
 
 # Given an array of integers nums which is sorted in ascending order, and an integer target.
 # write a function to search target in nums. 
@@ -10,3 +10,65 @@
 # -10^4 < nums[i], target < 104
 #All the integers in nums are unique.
 #nums is sorted in ascending order.
+
+# 1. State The problem clearly . Identify inputs and output formats
+    # - Transverse a sorted array(num2) and find the index of a specific item(target)
+
+    # Inputs
+    # nums = [1,3,5,7,8,12]
+    # target = 5 
+
+    # Expected Output
+    # index = 2
+
+# 2. Test Case Examples
+
+#Case 1: The target is in the array
+
+test = {
+    'input': {
+        'nums': [1,3,5,7,8,12],
+        'target': 5
+    },
+    'output': 2
+}
+
+#Case 2: The target is not in the array
+
+test = {
+    'input': {
+        'nums': [1,2,6,7,9,12],
+        'target': 10
+    },
+    'output': -1
+}
+
+#Case 3: The target is the first element
+
+test = {
+    'input': {
+        'nums': [10,12,23,44,46,67],
+        'target': 10
+    },
+    'output': 0
+}
+
+#Test 4: The target is the last element
+
+test = {
+    'input': {
+        'nums': [10,12,13,24,36,47],
+        'target': 47
+    },
+    'output': 5
+}
+
+#Test 5: Only in element in nums
+
+test = {
+    'input': {
+        'nums': [10],
+        'target': 10
+    },
+    'output': 0
+}
