2556b05133af00b4b59b8e5d3697fb3de27b7afd
The Meta <alexmeta517@gmail.com>
2023-02-22 19:20:36 +0300

    Solution

--- Binary Search/Questions/leet.py
+++ Binary Search/Questions/leet.py
@@ -23,52 +23,63 @@
 
 # 2. Test Case Examples
 
+tests = []
+
 #Case 1: The target is in the array
 
-test = {
+tests.append({
     'input': {
         'nums': [1,3,5,7,8,12],
         'target': 5
     },
     'output': 2
-}
+})
 
 #Case 2: The target is not in the array
 
-test = {
+tests.append({
     'input': {
         'nums': [1,2,6,7,9,12],
         'target': 10
     },
     'output': -1
-}
+})
 
 #Case 3: The target is the first element
 
-test = {
+tests.append({
     'input': {
         'nums': [10,12,23,44,46,67],
         'target': 10
     },
     'output': 0
-}
+})
 
 #Test 4: The target is the last element
 
-test = {
+tests.append({
     'input': {
         'nums': [10,12,13,24,36,47],
         'target': 47
     },
     'output': 5
-}
+})
 
 #Test 5: Only in element in nums
 
-test = {
+tests.append({
     'input': {
         'nums': [10],
         'target': 10
     },
     'output': 0
-}
+})
+
+
+    # 3. State the solution in plain english
+    #O(log N) - Binary Search : 
+        # 1. Find the most central index in nums
+        # 2. If the central most index is the matching target, stop the search else -
+        # 3. Depending on the comparison of the ascending numbers, deside if to move on with the right side or the left side nums
+        # 4. Repeat solution 2 and 3 until you find the target and display its index, else
+        # 5. Return -1 (Target not found)
