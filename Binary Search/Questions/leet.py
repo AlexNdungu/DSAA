6934553ecdd7efb94418fc14309ddc52b6c36b31
The Meta <alexmeta517@gmail.com>
2023-02-22 18:47:20 +0300

    Leet Code Question

---
+++ Binary Search/Questions/leet.py
@@ -0,0 +1,12 @@
+#LeetCode Question
+
+# Given an array of integers nums which is sorted in ascending order, and an integer target.
+# write a function to search target in nums. 
+# If target exists, then return its index. Otherwise, return -1.
+# You must write an algorithm with O(log n) runtime complexity. (Divide And Concour : Binary Search)
+
+#Constraints
+# 1 <= nums.length <= 104
+# -10^4 < nums[i], target < 104
+#All the integers in nums are unique.
+#nums is sorted in ascending order.
