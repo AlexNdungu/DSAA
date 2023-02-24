76840b713861ff17000f551cda945f88ecb6eb5a
The Meta <alexmeta517@gmail.com>
2023-02-24 17:05:28 +0300

    Binary Search Trees

---
+++ Binary Tree/Questions/w3.py
@@ -0,0 +1,4 @@
+# Write a Python program to create a Balanced Binary Search Tree (BST) 
+# using an array of elements where array elements are sorted in ascending order.
+
+the_array = [1, 2, 3, 4, 5, 6, 7]
