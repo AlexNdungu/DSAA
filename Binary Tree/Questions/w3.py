f82813925d52c60ba4502faa24f67afd0a3a2f79
The Meta <alexmeta517@gmail.com>
2023-02-24 17:19:32 +0300

    ReadMe update

--- Binary Tree/Questions/w3.py
+++ Binary Tree/Questions/w3.py
@@ -5,3 +5,9 @@ the_array = [1, 2, 3, 4, 5, 6, 7]
 
 #Lets create the TreeNode Class
 
+class TreeNode:
+
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
