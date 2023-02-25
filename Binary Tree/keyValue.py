95c7862f9de29bc575b04d418cf00a0343bc86c6
The Meta <alexmeta517@gmail.com>
2023-02-25 16:21:46 +0300

    BST Key value pairs

---
+++ Binary Tree/keyValue.py
@@ -0,0 +1,15 @@
+# Soring key value pairs
+
+class BSTNode():
+
+    def __init__(self, key, value = None):
+
+        self.key = key
+        self.value = value
+        self.left = None
+        self.right = None
+        self.parent = None
+
+
+tree = BSTNode()
+        
