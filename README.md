fc697754dd3f22dbd07feffe8c0741dc7808885b
The Meta <alexmeta517@gmail.com>
2023-03-02 14:42:38 +0300

    LCS Optimized With Dynamic Programming

--- README.md
+++ README.md
@@ -442,3 +442,28 @@ def lcs_memo(seq1,seq2):
     return recurse(0,0)
 
 ```
+
+**Some more optimization with dynamic programming**
+
+```
+
+def lcs_dp(seq1,seq2):
+
+    n1,n2 = len(seq1),len(seq2)
+
+    table = [[0 for x in range(n2 + 1)] for x in range(n1 + 1)]
+
+    for i in range(n1):
+
+        for j in range(n2):
+
+            if seq1[i] == seq2[j]:
+
+                table[i+1][j+1] = 1 + table[i][j]
+
+            else:
+                table[i+1][j+1] = max(table[i][j+1], table[i+1][j])
+
+    return table[-1][-1]
+
+```
--- Recursion and Dynamic Programing/longComSub.py
+++ Recursion and Dynamic Programing/longComSub.py
@@ -123,4 +123,26 @@ def lcs_memo(seq1,seq2):
 
 #print(lcs_recursive(t0['input']['seq1'],t0['input']['seq2']))
 
-evaluate_test_cases(lcs_memo,tests)
+
+
+def lcs_dp(seq1,seq2):
+
+    n1,n2 = len(seq1),len(seq2)
+
+    table = [[0 for x in range(n2 + 1)] for x in range(n1 + 1)]
+
+    for i in range(n1):
+
+        for j in range(n2):
+
+            if seq1[i] == seq2[j]:
+
+                table[i+1][j+1] = 1 + table[i][j]
+
+            else:
+                table[i+1][j+1] = max(table[i][j+1], table[i+1][j])
+
+    return table[-1][-1]               
+
+
+evaluate_test_cases(lcs_dp,tests)
